b32c85f84f0fe02355eabd3c2de004a6cc1b16fc
Kurt Guardia <kurtguardia@gmail.com>
2021-01-15 12:11:58 +0100

    Worked with AnimatePresence hoc

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,5 +1,5 @@
-import { motion } from 'framer-motion';
-import React from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import React, { useState } from 'react';
 
 const containerVariants = {
   hidden: {
@@ -29,6 +29,10 @@ const childVariants = {
 };
 
 const Order = ({ pizza }) => {
+  const [showTitle, setShowTitle] = useState(true);
+  setTimeout(() => {
+    setShowTitle(false);
+  }, 4000);
   return (
     <motion.div
       initial='hidden'
@@ -36,7 +40,12 @@ const Order = ({ pizza }) => {
       variants={containerVariants}
       className='container order'
     >
-      <h2>Thank you for your order :)</h2>
+      <AnimatePresence>
+        {showTitle && (
+          <motion.h2 exit={{ y: -1000 }}>Thank you for your order :)</motion.h2>
+        )}
+      </AnimatePresence>
+
       <motion.p variants={childVariants}>
         You ordered a {pizza.base} pizza with:
       </motion.p>
